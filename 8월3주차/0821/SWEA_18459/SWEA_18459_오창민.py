6913a73cb0f4d3e1ea3ee10e420c225bc095ecfa
ohchang <dhckdals1104@gmail.com>
2025-08-21 14:55:26 +0900

    solve : SWEA_18459_오창민 - BFS 연습

--- 8월3주차/0821/SWEA_18459/SWEA_18459_오창민.py
+++ 8월3주차/0821/SWEA_18459/SWEA_18459_오창민.py
@@ -14,11 +14,11 @@ def bfs(s, V):  # 시작정점 s, 마지막 정점 V
     print('#1',*lst)
 V, E = map(int, input().split()) # 1번부터 V번 정점, E개의 간선
 arr = list(map(int, input().split()))
-# 인접리스트 -------------------------
+# 인접리스트 ------------------------
 adj_l = [[] for _ in range(V+1)]
 for i in range(E):
     v1, v2 = arr[i*2], arr[i*2+1]
     adj_l[v1].append(v2)
     adj_l[v2].append(v1)    # 방향이 없는 경우
-# 여기까지 인접리스트 -----------------
+# 여기까지 인접리스트 ----------------
 bfs(1, 7)
