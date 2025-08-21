7ff939c7eedc1c349061bd83532b1884c5cba223
Ssongnya <alsrud9972@naver.com>
2025-08-21 23:58:18 +0900

    solving: SWEA_10966_송민경 - 물놀이 가자

---
+++ 8월3주차/0821/SWEA_10966/SWEA_10966_송민경.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+
+def find_water(arr, now):
+    delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+
+for t in range(1, T + 1):
+    N, M = map(int, input().split())
+
+    my_map = [list(map(int, input().strip())) for _ in range(N)]
+
