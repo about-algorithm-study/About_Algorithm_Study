209cbf273f5fc7dad30f213794f82fc2128bd640
배재유 <akakehcn@gmail.com>
2025-10-21 17:21:18 +0900

    solve: SWEA_2805_smk

---
+++ 10월2주차/SWEA_2805/SWEA_2805_smk.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input())
+
+    farm = [list(map(int, input().strip())) for _ in range(N)]
+    total = 0
+    if N == 1:
+        total += farm[0][0]
+        print(f"#{t} {total}")
+        continue
+
+    dist = (N-1)//2
+
+    for i in range(N):
+        for j in range(N):
+            if abs(i - dist) + abs(j - dist) <= dist:
+                total += farm[i][j]
+    
+    print(f"#{t} {total}")
